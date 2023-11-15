05e2dce45399b76462346f296dca4f3e8862b6d7
nrodriguez <nrodriguez@vantageio.com>
2023-11-15 18:11:59 -0500

    add code

--- src/Router/Components/Common/ProductCard.tsx
+++ src/Router/Components/Common/ProductCard.tsx
@@ -1,26 +1,25 @@
-import { Card, Button, CardBody, HStack,Image } from "@chakra-ui/react";
+import { Card, Button, CardBody, HStack, Image } from "@chakra-ui/react";
 import React from "react";
 import Typography from "./Typography";
 import CustomIcon from "../CustomIcon";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-const ProductCard = ({product}) => {
-  const {image, title, price, description} = product
-  
+const ProductCard = ({ product }) => {
+  const { images, title, price, description } = product;
+
+  const urlImage = images ? images[0] : ""
+
   return (
     <Card>
       <CardBody>
-        <Image src={image} alt="Dan Abramov" />
+        <Image src={urlImage} alt="Dan Abramov" />
         <Typography>{title}</Typography>
-          <Typography>{price}</Typography>
+        <Typography>{price}</Typography>
         <Typography>${description}</Typography>
         <HStack w="100%">
-          <Button w="70%">More Details</Button>
-          <Button>
-            <CustomIcon
-              icon={faCartShopping}
-              size="24px"
-            />
+          <Button w="80%">More Details</Button>
+          <Button ml="auto">
+            <CustomIcon icon={faCartShopping} size="24px" />
           </Button>
         </HStack>
       </CardBody>
