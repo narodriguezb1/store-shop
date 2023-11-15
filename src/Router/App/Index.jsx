f5ed481d3fc867c9f59cddbd1e10747b93cb29c5
nrodriguez <nrodriguez@vantageio.com>
2023-11-15 12:02:08 -0500

    add router

--- src/Router/App/Index.jsx
+++ src/Router/App/Index.jsx
@@ -1,26 +1,48 @@
-import './App.css'
-import Home from "../Home"
-import MyAccount from '../MyAccount'
-import MyOrder from '../MyOrder'
-import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
-import SignIn from '../SignIn'
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+import "./App.css";
 
-function App() {
+const InitialRoutes = () => {
+  let routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/my-order",
+      element: <MyOrder />,
+    },
+    {
+      path: "/my-orders",
+      element: <MyOrders />,
+    },
+    {
+      path: "/my-account",
+      element: <MyAccount />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "/*",
+      element: <NotFound />,
+    },
+  ]);
+  return routes;
+};
 
-
+const App = () => {
   return (
-    <>
-      <div className='bg-red-100'>
-        <Home/>
-        <MyAccount/>
-        <MyOrder/>
-        <MyOrders/>
-        <NotFound/>
-        <SignIn/>
-      </div>
-    </>
-  )
-}
+    <BrowserRouter>
+      <InitialRoutes />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
