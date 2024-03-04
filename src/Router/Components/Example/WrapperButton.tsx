b6fb3634fd0854dccd118050171905c7963e9dab
nrodriguez1 <nrodriguez@vantageio.com>
2024-03-04 18:19:36 -0500

    clear name

--- src/Router/Components/Example/WrapperButton.tsx
+++ src/Router/Components/Example/WrapperButton.tsx
@@ -2,7 +2,6 @@ import React, { memo } from "react";
 import { Button } from "@chakra-ui/react";
 
 const WrapperButton = ({onClick, label}) => {
-  console.log("hiiiiiiii button")
   return (
      <Button onClick={()=>{
       onClick()
