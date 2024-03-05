a34487575d50f2cebddc0a818f383ea6cb599391
nrodriguez1 <nrodriguez@vantageio.com>
2024-03-04 19:51:34 -0500

    new example

---
+++ src/Router/Components/Example/Hooks/BooleanComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../../Layout";
+import Typography from "../../Common/Typography";
+
+const BooleanComponent = () => {
+  const names = ["nestor", "alejandro", "david", "kevin"];
+
+  return (
+    <Layout>
+        <Typography>
+            hola
+        </Typography>
+    </Layout>
+  );
+};
+
+export default BooleanComponent;
--- src/Router/Components/Example/Performance/ExampleComponent.tsx
+++ src/Router/Components/Example/Performance/ExampleComponent.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Typography from "../../Common/Typography";
 import Layout from "../../Layout";
 import NamesGenerator from "./NamesGenerator";
 
